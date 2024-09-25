eb83d6af6b07c1a74972f7840f8d53a3f760a082
RetroMike <michaeldlesk@gmail.com>
2024-09-24 20:46:17 -0500

    initial code

---
+++ RMT-Stratum-Ping/Form1.Designer.cs
@@ -0,0 +1,134 @@
+﻿namespace RMT_Stratum_Ping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.stratumTextBox = new System.Windows.Forms.TextBox();
+            this.resultsTextBox = new System.Windows.Forms.TextBox();
+            this.executeButton = new System.Windows.Forms.Button();
+            this.resetButton = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(288, 36);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Stratum Server List";
+            // 
+            // stratumTextBox
+            // 
+            this.stratumTextBox.BackColor = System.Drawing.Color.Black;
+            this.stratumTextBox.ForeColor = System.Drawing.Color.Magenta;
+            this.stratumTextBox.Location = new System.Drawing.Point(17, 48);
+            this.stratumTextBox.Multiline = true;
+            this.stratumTextBox.Name = "stratumTextBox";
+            this.stratumTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.stratumTextBox.Size = new System.Drawing.Size(336, 405);
+            this.stratumTextBox.TabIndex = 1;
+            // 
+            // resultsTextBox
+            // 
+            this.resultsTextBox.BackColor = System.Drawing.Color.Black;
+            this.resultsTextBox.ForeColor = System.Drawing.Color.Lime;
+            this.resultsTextBox.Location = new System.Drawing.Point(370, 48);
+            this.resultsTextBox.Multiline = true;
+            this.resultsTextBox.Name = "resultsTextBox";
+            this.resultsTextBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.resultsTextBox.Size = new System.Drawing.Size(527, 489);
+            this.resultsTextBox.TabIndex = 2;
+            // 
+            // executeButton
+            // 
+            this.executeButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.executeButton.ForeColor = System.Drawing.Color.Purple;
+            this.executeButton.Location = new System.Drawing.Point(12, 481);
+            this.executeButton.Name = "executeButton";
+            this.executeButton.Size = new System.Drawing.Size(135, 53);
+            this.executeButton.TabIndex = 3;
+            this.executeButton.Text = "Execute";
+            this.executeButton.UseVisualStyleBackColor = true;
+            this.executeButton.Click += new System.EventHandler(this.executeButton_Click);
+            // 
+            // resetButton
+            // 
+            this.resetButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.resetButton.ForeColor = System.Drawing.Color.Red;
+            this.resetButton.Location = new System.Drawing.Point(165, 481);
+            this.resetButton.Name = "resetButton";
+            this.resetButton.Size = new System.Drawing.Size(135, 53);
+            this.resetButton.TabIndex = 4;
+            this.resetButton.Text = "Reset";
+            this.resetButton.UseVisualStyleBackColor = true;
+            this.resetButton.Click += new System.EventHandler(this.resetButton_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(365, 9);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(98, 29);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Results";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(909, 546);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.resetButton);
+            this.Controls.Add(this.executeButton);
+            this.Controls.Add(this.resultsTextBox);
+            this.Controls.Add(this.stratumTextBox);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox stratumTextBox;
+        private System.Windows.Forms.TextBox resultsTextBox;
+        private System.Windows.Forms.Button executeButton;
+        private System.Windows.Forms.Button resetButton;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ RMT-Stratum-Ping/Form1.cs
@@ -0,0 +1,186 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net.Sockets;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices.WindowsRuntime;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.ProgressBar;
+using System.IO;
+using System.Net.Security;
+using System.Threading;
+using System.Diagnostics;
+
+namespace RMT_Stratum_Ping
+{
+    public partial class Form1 : Form
+    {
+        bool ipv6 = false;
+        int count = 5;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            stratumTextBox.Text = "stratum+tcp://us.kaspa.herominers.com:1208";
+        }
+
+        private void resetButton_Click(object sender, EventArgs e)
+        {
+            stratumTextBox.Text = String.Empty;
+            resultsTextBox.Text = String.Empty;
+        }
+
+        private void executeButton_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(stratumTextBox.Text))
+            {
+                MessageBox.Show("At least one stratum endpoint must be specified");
+                return;
+            }
+
+            Process();
+        }
+
+        async Task Process()
+        {
+            var lines = stratumTextBox.Text.Split(new string[] { "\r\n" }, StringSplitOptions.RemoveEmptyEntries);
+            if (lines.Length > 0)
+            {
+                foreach (var line in lines)
+                {
+                    await Run(line);
+                }
+            }
+        }
+
+        async Task Run(string poolStratum)
+        {
+            try
+            {
+                var proto = "stratum2";
+                var uri = new Uri(poolStratum);
+
+                var scheme = uri.Scheme;
+                var host = uri.Host;
+                var port = uri.Port;
+                var isTls = false;
+
+                var addr = await ResolveAsync(host);
+                switch (scheme)
+                {
+                    case "stratum+tls":
+                        isTls = true;
+                        break;
+                    case "stratum+eth":
+                        proto = "stratum1";
+                        break;
+                    default:
+                        break;
+                }
+
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("PING stratum {0} ({1}) {3} port {2}", host, addr, port, isTls ? "TLS" : String.Empty);
+
+                TimeSpan min = TimeSpan.FromHours(1);
+                TimeSpan max = TimeSpan.Zero;
+                TimeSpan avg = TimeSpan.Zero;
+                int avgCount = 0;
+                int success = 0;
+                DateTime start = DateTime.Now;
+                for (int i = 0; i < count; i++)
+                {
+                    try
+                    {
+                        TimeSpan elapsed = await DoPingAsync(addr, isTls, host, port, proto);
+                        resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("{0} ({1}): seq={2}, time={3}", host, addr, i, elapsed.Milliseconds);
+                        if (elapsed > max)
+                            max = elapsed;
+                        if (elapsed < min)
+                            min = elapsed;
+                        avg += elapsed;
+                        avgCount++;
+                        success++;
+                    }
+                    catch (Exception ex)
+                    {
+                        resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("{0} ({1}): seq={2}, {3}", host, addr, i, ex.Message);
+                    }
+                    await Task.Delay(1000);
+                }
+
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("\r\n--- {0} ping statistics ---", host);
+                int loss = 100 - (int)(((double)success / count) * 100.0);
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("{0} packets transmitted, {1} received, {2}% packet loss, time {3}", count, success, loss, DateTime.Now - start);
+                if (success > 0)
+                {
+                    resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("min/avg/max = {0} ms, {1} ms, {2} ms", min.Milliseconds, TimeSpan.FromTicks(avg.Ticks / avgCount).Milliseconds, max.Milliseconds);
+                }
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("\r\n--------------\r\n");
+            }
+            catch(Exception ex)
+            {
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("\r\n--------------\r\n");
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + ex.Message + " - " + poolStratum;
+                resultsTextBox.Text = resultsTextBox.Text + "\r\n" + String.Format("\r\n--------------\r\n");
+            }
+        }
+
+        private async Task<IPAddress> ResolveAsync(string host)
+        {
+            AddressFamily family = ipv6 ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork;
+            IPHostEntry hostEntry = await Dns.GetHostEntryAsync(host);
+            var addr = Array.Find(hostEntry.AddressList, a => a.AddressFamily == family);
+            if (addr == null)
+            {
+                throw new Exception($"Failed to resolve host name: {host}");
+            }
+            return addr;
+        }
+
+        private async Task<TimeSpan> DoPingAsync(IPAddress addr, bool tls, string host, int port, string proto)
+        {
+            string dial = ipv6 ? $"[{addr}]:{port}" : $"{addr}:{port}";
+            using (TcpClient client = new TcpClient(ipv6 ? AddressFamily.InterNetworkV6 : AddressFamily.InterNetwork))
+            {
+
+                DateTime start = DateTime.Now;
+                await client.ConnectAsync(addr, port);
+
+                Stream stream = client.GetStream();
+                if (tls)
+                {
+                    SslStream sslStream = new SslStream(stream, false, (sender, certificate, chain, errors) => true);
+                    await sslStream.AuthenticateAsClientAsync(host);
+                    stream = sslStream;
+                }
+
+                using (StreamWriter writer = new StreamWriter(stream, Encoding.ASCII, 1024, true))
+                {
+                    var requestData = "";
+                    switch (proto)
+                    {
+                        case "stratum1":
+                            requestData = "{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"eth_submitLogin\",\"params\":[\"login\",\"pass\"]";
+                            break;
+                        case "stratum2":
+                            requestData = "{\"id\":1,\"jsonrpc\":\"2.0\",\"method\":\"mining.subscribe\",\"params\":[\"stratum-ping/1.0.0\",\"EthereumStratum/1.0.0\"]";
+                            break;
+                        default:
+                            break;
+                    }
+                    writer.Write(requestData);
+                }
+                return DateTime.Now - start;
+
+            }
+        }
+    }
+}
